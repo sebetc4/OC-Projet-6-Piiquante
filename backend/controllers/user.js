d95284d49a36185c96fead04a28dc444bf8eaad9
seb etc <seb-etc@hotmail.fr>
2022-03-25 18:12:08 +0100

    test

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -32,7 +32,7 @@ exports.login = (req, res, next) => {
                 .compare(req.body.password, user.password)
                 .then((valid) => {
                     if (!valid) {
-                        return res.status(401).json({ error: "Mot de passe incorrect!" });
+                        return res.status(403).json({ error: "Mot de passe incorrect!" });
                     }
                     res.status(200).json({
                         userId: user._id,
